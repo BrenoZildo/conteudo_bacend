e75dc45e3f26391cf44f5fe677b204cfec32b9e7
BrenoZildo <breno1.vieira@alunos.ifsuldeminas.edu.br>
2023-04-05 10:38:26 -0300

    aula14

--- aula12/ler.php
+++ aula12/ler.php
@@ -5,16 +5,12 @@
 <?php
     $id= $_GET["id"];
     $noticia = $noticias[$id];
-
-
-
 ?>
-
 
 <div class="container">
     <h1> <?php echo $noticia['titulo']; ?> </h1>
     <hr>
-    <img src=" <?php echo $noticia['foto']; ?>" class=""/>
+    <img src="<?php echo $noticia['foto']; ?>" class=""/>
     <p> <?php echo $noticia['materia']; ?> </p>
 </div>
 <?php  include_once "rodape.php" ?>
